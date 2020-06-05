18bce66cdbe51a1a36304e41aa829c00e7d17c73
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-06-05 12:19:09 +0200

    half sql server

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        private DbSet<Product> products;
+        private DbSet<ProductCategory> productsCategories;
+        public DataContext() : base("DefaultConnection") { 
+        }
+        public DbSet<Product> Products {
+            get {return this.products ;}
+            set {this.products = value; } 
+        }
+        public DbSet<ProductCategory> ProductsCategories
+        {
+            get { return this.ProductsCategories; }
+            set { this.ProductsCategories = value; }
+        }
+    }
+}
