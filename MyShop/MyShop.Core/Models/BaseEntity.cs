5023768c9d0453f39b50ab4d8753bb1da5bd6bd6
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-06-05 13:35:22 +0200

    Fixin' the cluster fuck that entity framework really is

--- MyShop/MyShop.Core/Models/BaseEntity.cs
+++ MyShop/MyShop.Core/Models/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,17 +9,13 @@ namespace MyShop.Core.Models
 {
     public abstract class BaseEntity
     {
-        private string id;
-        private DateTimeOffset createdAt;
+        [Key]
+        public string Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
         public BaseEntity() {
-            this.id = Guid.NewGuid().ToString();
-            this.createdAt = DateTime.Now;
+            this.Id = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
         }
-        public string Id{
-            get { return this.id; }
-        }
-        public DateTimeOffset CreatedAt{
-            get { return this.createdAt; }
-        }
+
     }
 }
--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -10,18 +11,10 @@ namespace MyShop.DataAccess.SQL
 {
     public class DataContext : DbContext
     {
-        private DbSet<Product> products;
-        private DbSet<ProductCategory> productsCategories;
-        public DataContext() : base("DefaultConnection") { 
+        public DataContext() : base("DefaultConnection") {
         }
-        public DbSet<Product> Products {
-            get {return this.products ;}
-            set {this.products = value; } 
-        }
-        public DbSet<ProductCategory> ProductsCategories
-        {
-            get { return this.ProductsCategories; }
-            set { this.ProductsCategories = value; }
-        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
     }
 }
